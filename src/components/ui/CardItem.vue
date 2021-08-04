1b36fad5e1c58e7c84d6694daa86b58cbb1426d5
Volodymyr Burkovskiy <volodymyr.b.pro@gmail.com>
2021-08-04 11:15:40 +0300

    Fix button

--- src/components/ui/CardItem.vue
+++ src/components/ui/CardItem.vue
@@ -7,10 +7,20 @@
     <div class="card-item__quantity">{{ quantity }}</div>
     <h3 class="card-item__title">{{ title }}</h3>
     <button class="card-item__button button">
-      <div class="button__title">Купить</div>
-      <div class="button__price-wrapper">
-        <div class="button__icon"></div>
-        <div class="button__price">{{ price }}</div>
+      <div class="button__wrapper">
+        <div class="button__title">Купить</div>
+        <div class="button__price-wrapper">
+          <div class="button__icon">
+            <picture>
+              <source media="(min-width: 1366px" srcset="@/assets/img/$.svg" />
+              <img
+                src="@/assets/img/icon-crystals-mobile.png"
+                alt="icon currency"
+              />
+            </picture>
+          </div>
+          <div class="button__price">{{ price }}</div>
+        </div>
       </div>
     </button>
     <div class="ribbons" :class="{ hidden: !bonus }">
@@ -37,6 +47,7 @@ export default {
   background: url("../../assets/img/bg-card.png") center center/cover no-repeat;
   list-style: none;
   overflow: hidden;
+  text-align: center;
   &__img {
     display: block;
     margin: 0 auto;
@@ -55,22 +66,16 @@ export default {
     color: #fff;
   }
 }
+
 .button {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   margin: 7px auto 0;
-  padding: 0 10px;
   width: 98px;
   height: 27px;
   border: none;
   outline: none;
   border-radius: 146px;
-  font-weight: bold;
-  font-size: 11px;
-  color: #ffffff;
-  background: linear-gradient(90deg, #682fbd 0%, #cb4cbc 100%);
   cursor: pointer;
+  background: linear-gradient(90deg, #682fbd 0%, #cb4cbc 100%);
   &__price-wrapper {
     display: flex;
     align-items: center;
@@ -78,37 +83,47 @@ export default {
   &__icon {
     width: 17px;
     height: 17px;
-    background: url("../../assets/img/icon-crystals-mobile.png") center
-      center/cover no-repeat;
   }
+  &__price,
+  &__title {
+    font-weight: bold;
+    font-size: 11px;
+    color: #ffffff;
+  }
+}
+.button__wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
+}
 
-  .ribbons {
-    position: absolute;
-    top: 10px;
-    left: -30px;
-    transform: rotate(-45deg);
-    width: 100px;
-    height: 24px;
-    background: linear-gradient(53.76deg, #ff4910 10.28%, #ff4910 87.14%);
-    &__price {
-      position: relative;
-      padding-left: 14px;
-      font-weight: bold;
-      font-size: 11px;
-      line-height: 24px;
-      text-align: center;
-      color: #ffffff;
-      &::before {
-        position: absolute;
-        top: 4px;
-        left: 30px;
-        content: "";
-        display: inline-block;
-        width: 14px;
-        height: 14px;
-        background: url("../../assets/img/icon-one-coin-mobile.png") center
-          center/cover no-repeat;
-      }
+.ribbons {
+  position: absolute;
+  top: 10px;
+  left: -30px;
+  transform: rotate(-45deg);
+  width: 100px;
+  height: 24px;
+  background: linear-gradient(53.76deg, #ff4910 10.28%, #ff4910 87.14%);
+  &__price {
+    position: relative;
+    padding-left: 14px;
+    font-weight: bold;
+    font-size: 11px;
+    line-height: 24px;
+    text-align: center;
+    color: #ffffff;
+    &::before {
+      position: absolute;
+      top: 4px;
+      left: 30px;
+      content: "";
+      display: inline-block;
+      width: 14px;
+      height: 14px;
+      background: url("../../assets/img/icon-one-coin-mobile.png") center
+        center/cover no-repeat;
     }
   }
 }
@@ -139,24 +154,43 @@ export default {
       line-height: 22px;
     }
   }
+
   .button {
-    margin: 24px auto 0;
-    padding: 0 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 24px;
     width: 154px;
-    height: 48px;
-    font-size: 14px;
-    line-height: 16px;
-    background: linear-gradient(90deg, #242333 0%, #3c3b53 100%);
-    &__wrapper {
-      background: linear-gradient(90deg, #682fbd 0%, #cb4cbc 100%);
+    height: 47px;
+    border: none;
+    outline: none;
+    border-radius: 146px;
+    cursor: pointer;
+    background: linear-gradient(90deg, #682fbd 0%, #cb4cbc 100%);
+    &__price-wrapper {
+      display: flex;
+      align-items: center;
     }
     &__icon {
-      margin-right: 4px;
       width: 8px;
-      height: 14px;
-      background-image: url("../../assets/img/$.svg");
+      height: 15px;
+    }
+    &__price,
+    &__title {
+      font-size: 14px;
+    }
+    &__price {
+      margin-left: 4px;
     }
   }
+  .button__wrapper {
+    padding: 0 30px;
+    width: 150px;
+    height: 43px;
+    background: linear-gradient(90deg, #242333 0%, #3c3b53 100%);
+    border-radius: 146px;
+  }
+
   .ribbons {
     width: 120px;
     height: 30px;
